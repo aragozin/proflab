543ce0211519fe07f15df966b372fa4179a27585
Alexey Ragozin <alexey.ragozin@gmail.com>
2019-02-05 01:27:32 +0300

    A bit more polished example

---
+++ commands.md
@@ -0,0 +1,17 @@
+﻿Simple example doing crypto hash calculation to illustrate sampling bias.
+
+Start process with default JVM settings
+
+    java -cp target/classes CryptoBench
+
+Start process with extra debug symblos (for Flight Recorder Sampling)
+
+    java -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -cp target/classes CryptoBench
+
+Start process for profiling with perf-map
+
+    java -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -XX:+PreserveFramePointer -cp target/classes CryptoBench
+
+Start with tracing in bench loop
+
+    java -cp target/classes -DtrackTime=true CryptoBench
---
+++ src/main/java/CryptoBench.java
@@ -0,0 +1,64 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
+
+public class CryptoBench {
+
+	private static final boolean trackTime = Boolean.getBoolean("trackTime");
+	
+	public static void main(String[] args) {
+		CryptoBench test = new CryptoBench();
+		while(true) {
+			test.execute();
+		}		
+	}
+	
+	public void execute() {
+        long N = 5 * 1000 * 1000;
+        RandomStringUtils randomStringUtils = new RandomStringUtils();
+        long ts = 0,tf = 0;
+        long timer1 = 0;
+        long timer2 = 0;
+        long bs = System.nanoTime();
+        for (long i = 0; i < N; i++) {
+        	ts = trackTime ? System.nanoTime() : 0;
+            String text = randomStringUtils.generate();
+            tf = trackTime ? System.nanoTime() : 0;
+            timer1 += tf - ts;
+            ts = tf;
+			crypt(text);
+			tf = trackTime ? System.nanoTime() : 0;
+			timer2 += tf - ts;
+			ts = tf;
+        }
+        long bt = System.nanoTime() - bs;
+        System.out.print(String.format("Hash rate: %.2f Mm/s", 0.01 * (N * TimeUnit.SECONDS.toNanos(1) / bt / 10000)));
+        if (trackTime) {
+	        System.out.print(String.format(" | Generation: %.1f %%",  0.1 * (1000 * timer1 / (timer1 + timer2))));
+	        System.out.print(String.format(" | Hasing: %.1f %%", 0.1 * (1000 * timer2 / (timer1 + timer2))));
+        }
+        System.out.println();
+	}
+
+    public String crypt(String str) {
+        if (str == null || str.length() == 0) {
+            throw new IllegalArgumentException("String to encript cannot be null or zero length");
+        }
+        StringBuilder hexString = new StringBuilder();
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes());
+            byte[] hash = md.digest();
+            for (byte aHash : hash) {
+                if ((0xff & aHash) < 0x10) {
+                    hexString.append("0" + Integer.toHexString((0xFF & aHash)));
+                } else {
+                    hexString.append(Integer.toHexString(0xFF & aHash));
+                }
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return hexString.toString();
+    }
+}
